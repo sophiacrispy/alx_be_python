d247ac0d3eaac143557007a329ad46d03a2fa4cf
sophiacrispy <sophiacrispy1@gmail.com>
2025-02-26 18:22:43 +0100

    git branch

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
